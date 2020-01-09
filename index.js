3ed12be81edca30bfc00d790db706f52855d98bd
woubuc <wouter@epicteddy.com>
2020-01-09 21:43:12 +0100

    🐛 Match on request path instead of full url

--- index.js
+++ index.js
@@ -95,22 +95,22 @@ function fileExists(file) {
 	let redirects = await getRedirects(config.publish);
 
 	async function handler(ctx) {
-		if (ctx.url.endsWith('/')) {
-			ctx.url = ctx.url.slice(0, -1);
+		if (ctx.path.endsWith('/')) {
+			ctx.path = ctx.path.slice(0, -1);
 		}
 
 		for (let add of ['', '.html', '/index.html']) {
-			let exists = await fileExists(path.join(config.publish, ctx.url + add));
+			let exists = await fileExists(path.join(config.publish, ctx.path + add));
 			if (!exists) continue;
 
-			console.log('%s', ctx.url + add);
+			console.log('%s', ctx.path + add);
 
-			await send(ctx, ctx.url + add, { root: config.publish });
+			await send(ctx, ctx.path + add, { root: config.publish });
 			return;
 		}
 
 		for (let rule of redirects) {
-			let matched = rule.match(ctx.url);
+			let matched = rule.match(ctx.path);
 			if (matched === false) continue;
 
 			let to = rule.to;
@@ -118,7 +118,7 @@ function fileExists(file) {
 				to = to.replace(`:${ key }`, value);
 			}
 
-			console.log('%s -> %s (%d)', ctx.url, to, rule.status);
+			console.log('%s -> %s (%d)', ctx.path, to, rule.status);
 
 			if (rule.status === 301 || rule.status === 302) {
 				ctx.status = rule.status;
@@ -126,7 +126,7 @@ function fileExists(file) {
 				return;
 			}
 
-			ctx.url = to;
+			ctx.path = to;
 			ctx.status = rule.status;
 			await handler(ctx, true);
 			return;
@@ -140,7 +140,7 @@ function fileExists(file) {
 	const app = new Koa();
 	app.use(handler);
 
-	const port = await getPort({ port: 9000 });
+	const port = await getPort({ port: getPort.makeRange(9000, 9999) });
 	await app.listen(port);
 
 	console.log(boxen(`Dev server listening on http://localhost:${ port }/`, {
